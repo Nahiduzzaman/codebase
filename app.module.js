0b49d9ab34d8891bfcc7ae30e75e6e73d08d330e
DESKTOP-2OU1GPU\nahid <nahiduzzaman@sgcsoft.net>
2017-03-02 02:19:26 +0600

    first commit

---
+++ app.module.js
@@ -0,0 +1,4 @@
+﻿(function (angular) {
+    'use strict';
+    angular.module('starterApp', ['ui.router','ngMaterial','md.data.table']);
+})(window.angular);
---
+++ controllers/about.controller.js
@@ -0,0 +1,14 @@
+﻿(function (angular) {
+'use strict';
+
+	angular.module('starterApp')
+	    .controller('aboutController', constructor);
+	    	constructor.$inject = ['SampleService','$scope','$timeout'];
+	    	function constructor(SampleService,$scope,$timeout) {
+	    		var vm = this;
+	    		vm.teamList = SampleService.getTeams();
+	    		console.log(vm.teamList);
+	        	
+	    	}
+
+})(window.angular);
