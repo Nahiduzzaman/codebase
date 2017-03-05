dc5bd37c6f453e6eb4a3346c4fbb3753d2e9dc9a
DESKTOP-2OU1GPU\nahid <nahiduzzaman@sgcsoft.net>
2017-03-06 01:17:13 +0600

    hello map

--- controllers/about.controller.js
+++ controllers/about.controller.js
@@ -8,6 +8,25 @@
 	    		var vm = this;
 	    		vm.teamList = SampleService.getTeams();
 	    		console.log(vm.teamList);
+
+	    		window.initMap = function() {
+	    			console.log('hello')
+			        var uluru = {lat: -25.363, lng: 131.044};
+			        var map = new google.maps.Map(document.getElementById('map'), {
+			          zoom: 4,
+			          center: uluru
+			        });
+
+			        //
+			        $scope.marker1 = new google.maps.Marker({
+			          position: uluru,
+			          map: map,
+			          title: 'First Marker'
+
+			        })
+			    }
+
+
 	        	
 	    	}
 
